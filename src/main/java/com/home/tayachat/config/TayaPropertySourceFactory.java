a97d60eef9038b551cb4ed17360a68db7fd271fd
sergey.kochetov@am-intech.ru <inc4mest>
2022-04-13 00:10:31 +0300

    props change

--- src/main/java/com/home/tayachat/config/TayaPropertySourceFactory.java
+++ src/main/java/com/home/tayachat/config/TayaPropertySourceFactory.java
@@ -1,25 +1,27 @@
 package com.home.tayachat.config;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class TayaPropertySourceFactory implements PropertySourceFactory {
     public TayaPropertySourceFactory() {
     }
 
-    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) throws IOException {
+    public @NonNull PropertySource<?> createPropertySource(@Nullable String name, @NonNull EncodedResource resource) throws IOException {
         Properties propertiesFromYaml = this.loadYamlIntoProperties(resource);
-        String sourceName = name != null ? name : resource.getResource().getFilename();
+        name = name != null ? name : resource.getResource().getFilename();
+        name  = name != null ? name : "";
         propertiesFromYaml.putAll(this.loadSystemProperties());
-        return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+        return new PropertiesPropertySource(name, propertiesFromYaml);
     }
 
     private Properties loadSystemProperties() {
@@ -29,15 +31,15 @@ public class TayaPropertySourceFactory implements PropertySourceFactory {
     private Properties loadYamlIntoProperties(EncodedResource resource) throws FileNotFoundException {
         try {
             YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
-            factory.setResources(new Resource[]{resource.getResource()});
+            factory.setResources(resource.getResource());
             factory.afterPropertiesSet();
             return factory.getObject();
-        } catch (IllegalStateException var4) {
-            Throwable cause = var4.getCause();
+        } catch (IllegalStateException ex) {
+            Throwable cause = ex.getCause();
             if (cause instanceof FileNotFoundException) {
-                throw (FileNotFoundException)var4.getCause();
+                throw (FileNotFoundException) ex.getCause();
             } else {
-                throw var4;
+                throw ex;
             }
         }
     }
